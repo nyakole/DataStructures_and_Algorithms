208584309481c77f89a939de4e828e4cf45ffd49
nyakole <akolenikhil@gmail.com>
2023-08-30 18:43:09 +0530

    561 completed

---
+++ HeyCoach/FoundationQuestions/561.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int arrayPairSum(vector<int> &nums)
+{
+    int sum = 0;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i = i + 2)
+    {
+        sum += nums[i];
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+
+    int result = arrayPairSum(nums);
+    cout << "Output: " << result << endl;
+    return 0;
+}
